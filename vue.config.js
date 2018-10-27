2e816fa5e16fb929b1d55921c90fd5611944c4b7
Ryan Durham <ryan@stagerightlabs.com>
2018-10-26 21:00:16 -0700

    Install tailwind, sass and purgecss

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const whitelister = require('purgecss-whitelister');
+const glob = require('glob-all');
+const path = require('path');
+
+class TailwindExtractor {
+  static extract (content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
+module.exports = {
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './**/*.vue'),
+          path.join(__dirname, './src/**/*.js'),
+        ]),
+        whitelist: whitelister(['./src/assets/pikaday.css']),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ['html', 'js', 'php', 'vue'],
+          },
+        ],
+      }),
+    ],
+  },
+};
