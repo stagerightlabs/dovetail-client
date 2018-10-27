d6a3fd3146d90535158e653eb911521069bcfa8d
Ryan Durham <ryan@stagerightlabs.com>
2018-10-26 21:00:27 -0700

    Initial styling for login page

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,16 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
+@import './assets/styles.scss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  @apply h-screen;
 }
 #nav {
   padding: 30px;
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="absolute pin mx-auto h-full flex justify-center items-center">
+    <div class="card">
+      <div class="mb-4">
+        <label>Email:</label>
+        <input type="text">
+      </div>
+      <div class="mb-4">
+        <label>Password:</label>
+        <input type="password">
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="btn btn-teal">Login</button>
+        <a class="btn btn-link" href="#">Forgot Password?</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+
+})
+</script>
+
+<style>
+
+</style>
