2a7a616c0280033efa58a27c2cfdeb07f4bef61b
Ryan C. Durham <ryan@stagerightlabs.com>
2018-11-17 16:21:14 -0800

    Add Organization, Team and User model types

--- src/types.ts
+++ src/types.ts
@@ -15,5 +15,21 @@ export interface AuthToken {
 export interface User {
     name: string;
     email: string;
+    email_verified_at?: string;
     phone?: string;
+    phone_verified_at?: string;
+    teams: Team[];
+}
+
+export interface Organization {
+    hashid: string;
+    name: string;
+    slug: string;
+    logo_url?: string;
+    configuration?: [];
+}
+
+export interface Team {
+    hashid: string;
+    name: string;
 }
